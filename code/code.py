87d68cfb493d3b526abf004283ef7799d77be5ca
vandensaunders-spec <vandensaunders@gmail.com>
2026-04-22 19:13:46 +1200

    scoring, snake and fruit

--- code/code.py
+++ code/code.py
@@ -1,6 +1,7 @@
 import turtle
 import time
 import random
+import pygame
 
 delay = 0.1
 
@@ -14,3 +15,32 @@ wn.title("Snake Game by Van")
 wn.bgcolor("green")
 wn.setup(width=600, height=600)
 wn.tracer(0) # Turns off the screen updates
+
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+# Snake food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0,100)
+
+segments = []
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
